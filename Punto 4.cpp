1bc696a3f029ce6ebebfb1d30dde863cfcfd69e7
Victor Efimov <victorefimov17@gmail.com>
2025-06-26 10:53:15 -0300

    Realizacion del Punto 4a

--- Punto 4.cpp
+++ Punto 4.cpp
@@ -1,1 +1,36 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct Pila {
+    pnodo tope;
+};
+
+void iniciar_pila(Pila &p) 
+{
+    p.tope = NULL;
+}
+
+void push(Pila &p, int valor) 
+{
+    pnodo nuevo;
+    crear_nodo(nuevo, valor);
+    nuevo->siguiente = p.tope;
+    p.tope = nuevo;
+}
+
+int pop(Pila &p) 
+{
+    if (p.tope == NULL) return -1;
+    pnodo aux = p.tope;
+    int dato = aux->dato;
+    p.tope = p.tope->siguiente;
+    delete aux;
+    return dato;
+}
+
+bool pila_vacia(Pila p) 
+{
+    return p.tope == NULL;
+}
 
