d2fed9b77ffeff217f360543eb694d12bdd745d2
Victor Efimov <victorefimov17@gmail.com>
2025-06-26 10:36:46 -0300

    Funcion para eliminar y devolver el numero del elemento

--- Punto 2.cpp
+++ Punto 2.cpp
@@ -29,3 +29,18 @@ void agregar_final(tlista &lista, pnodo nuevo)
     }
     lista.contador++;
 }
+pnodo eliminar_inicio(tlista &lista) 
+{
+    if (lista.inicio == NULL) return NULL;
+    pnodo borrado = lista.inicio;
+    lista.inicio = lista.inicio->siguiente;
+    if (lista.inicio == NULL) lista.final = NULL;
+    borrado->siguiente = NULL;
+    lista.contador--;
+    return borrado;
+}
+
+int cantidad_elementos(tlista lista) 
+{
+    return lista.contador;
+}
