ec677ab0b5c6c0eb0c03c598a66da4f5753800ec
Victor Efimov <victorefimov17@gmail.com>
2025-06-26 16:02:19 -0300

    Realización del Punto 6a

--- Punto 6.cpp
+++ Punto 6.cpp
@@ -1,1 +1,26 @@
+#include<bits/stdc++.h>
 
+using namespace std;
+
+bool es_primo(int n) 
+{
+    if (n <= 1)
+    { 
+      return false;
+    }
+    for (int i = 2; i*i <= n; ++i)
+        if (n % i == 0) 
+        {
+          return false;
+        }
+    return true;
+}
+int contar_primos(pnodo lista)
+{
+    int contador = 0;
+    for (pnodo i = lista; i != NULL; i = i->siguiente) 
+    {
+        if (es_primo(i->dato)) contador++;
+    }
+    return contador;
+}
