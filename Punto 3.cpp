ed36b5cae24e5d81de0fbd6275a1bd821f25076b
Victor Efimov <victorefimov17@gmail.com>
2025-06-26 10:48:04 -0300

    Funcion que devuelve el entero

--- Punto 3.cpp
+++ Punto 3.cpp
@@ -9,5 +9,14 @@ int buscar_indice(tlista lista, int valor)
     {
         if (i->dato == valor) return indice;
     }
-    return -999;
+    return -1;
+}
+pnodo buscar_por_indice(tlista lista, int h) 
+{
+    int indice = 0;
+    for (pnodo i = lista.inicio; i != NULL; i = i->siguiente, indice++) 
+    {
+        if (indice == h) return i;
+    }
+    return NULL;
 }
