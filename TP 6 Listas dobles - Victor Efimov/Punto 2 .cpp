707e42faf81a180192b6959542bffe1e4949604e
Victor Efimov <victorefimov17@gmail.com>
2025-07-01 22:54:29 -0300

    Realización del punto 2

--- TP 6 Listas dobles - Victor Efimov/Punto 2 .cpp
+++ TP 6 Listas dobles - Victor Efimov/Punto 2 .cpp
@@ -1,1 +1,54 @@
+#include <iostream>
+using namespace std;
 
+typedef struct tnodo *pnodo;
+
+struct tnodo {
+    int dato;
+    pnodo sig;
+    pnodo ant;
+};
+
+struct tlista {
+    pnodo inicio;
+    pnodo fin;
+};
+
+void iniciar_lista(tlista &lista) {
+    lista.inicio = NULL;
+    lista.fin = NULL;
+}
+
+pnodo eliminar_inicio(tlista &lista) {
+    if (lista.inicio == NULL) return NULL;
+    pnodo eliminado = lista.inicio;
+
+    if (lista.inicio == lista.fin) {
+        lista.inicio = NULL;
+        lista.fin = NULL;
+    } else {
+        lista.inicio = eliminado->sig;
+        lista.inicio->ant = NULL;
+    }
+
+    eliminado->sig = NULL;
+    eliminado->ant = NULL;
+    return eliminado;
+}
+
+pnodo eliminar_final(tlista &lista) {
+    if (lista.fin == NULL) return NULL;
+    pnodo eliminado = lista.fin;
+
+    if (lista.inicio == lista.fin) {
+        lista.inicio = NULL;
+        lista.fin = NULL;
+    } else {
+        lista.fin = eliminado->ant;
+        lista.fin->sig = NULL;
+    }
+
+    eliminado->sig = NULL;
+    eliminado->ant = NULL;
+    return eliminado;
+}
